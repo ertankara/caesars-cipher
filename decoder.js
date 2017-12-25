a487adff4b493c36c58b49f238fccc8bcfd1157c
Ertan Kara <kara.ertan9@gmail.com>
2017-12-25 23:12:13 +0300

    Carry all logic to the head

--- decoder.js
+++ decoder.js
@@ -1,28 +1,31 @@
+
 function decoder(str) { // LBH QVQ VG!
-  var encCharArray = str.split("");
-  var decUnicode = encCharArray.map(function(element) {
-    if (element.charCodeAt() >= "A".charCodeAt() &&
-        element.charCodeAt() <= "Z".charCodeAt()) {
+var encCharArray = str.split("");
+var decUnicode = encCharArray.map(function(element) {
+  if (element.charCodeAt() >= "A".charCodeAt() &&
+      element.charCodeAt() <= "Z".charCodeAt()) {
 
-      if (element != " " ||
-         (element.charCodeAt() < "A".charCodeAt() &&
-         element.charCodeAt() > "Z".charCodeAt())) {
+    if (element != " " ||
+       (element.charCodeAt() < "A".charCodeAt() &&
+       element.charCodeAt() > "Z".charCodeAt())) {
 
-        if (element.charCodeAt() - 13 < "A".charCodeAt()) {
-          return "Z".charCodeAt() - ( 12 - (element.charCodeAt() - "A".charCodeAt()));
-        }
-        else if ((element.charCodeAt() - 13) >= "A".charCodeAt()) {
-          return element.charCodeAt() - 13;
-        }
+      if (element.charCodeAt() - 13 < "A".charCodeAt()) {
+        return "Z".charCodeAt() - ( 12 - (element.charCodeAt() - "A".charCodeAt()));
       }
-      else {
-        return " ".charCodeAt();
+      else if ((element.charCodeAt() - 13) >= "A".charCodeAt()) {
+        return element.charCodeAt() - 13;
       }
     }
-    return element.charCodeAt();
-  });
-  var decCharArray = decUnicode.map(function(element) {
-    return String.fromCharCode(element);
-  });
-  return decCharArray.join("");
+    else {
+      return " ".charCodeAt();
+    }
+  }
+  return element.charCodeAt();
+});
+var decCharArray = decUnicode.map(function(element) {
+  return String.fromCharCode(element);
+});
+alert(decCharArray.join(""));
+// return decCharArray.join("");
+
 }
